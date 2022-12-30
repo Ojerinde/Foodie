45dc8835d1b53e50d44c47dd4c4b3413195cac1a
Joel Ojerinde <Joelojerinde@gmail.com>
2022-12-30 08:09:53 +0100

    Delete src directory

--- src/js/config.js
+++
@@ -1,6 +0,0 @@
-// Reusable variables
-export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
-export const TIMEOUT__SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = `0ff24345-7265-4f6c-b94a-5a99bc410ddd`;
-export const MODAL_CLOSE_SEC = 2.5;
